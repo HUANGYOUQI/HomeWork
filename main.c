8126b0f52611ad925e22a3d6f401c85ceda0e74f
HUANGYOUQI <kevin88307@gmail.com>
2018-05-18 22:46:33 +0800

    HomeWork

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int HomeWork,SumAll;
+int HomeWork,SumAll;/*變數宣告*/ 
 int main(int argc, char *argv[]) {
-	for(HomeWork=0;HomeWork<10;HomeWork++){
-		SumAll=SumAll+HomeWork;
+	for(HomeWork=0;HomeWork<10;HomeWork++)/*For迴圈從0到9*/
+	{
+		SumAll=SumAll+HomeWork;/*計算從0加到9*/ 
 	}
-	printf("SumAll=%d",SumAll);
+	printf("SumAll=%d",SumAll);/*印出總和*/ 
 	return 0;
 }
