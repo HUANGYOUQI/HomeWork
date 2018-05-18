19a2876ed8e719379f9e3f591ca77b8e9f99ea2d
HUANGYOUQI <kevin88307@gmail.com>
2018-05-18 22:42:42 +0800

    HomeWork

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int homework,sumall;
+int HomeWork,SumAll;
 int main(int argc, char *argv[]) {
-	for(homework=0;homework<10;homework++){
-		sumall=sumall+homework;
+	for(HomeWork=0;HomeWork<10;HomeWork++){
+		SumAll=SumAll+HomeWork;
 	}
-	printf("sumall=%d",sumall);
+	printf("SumAll=%d",SumAll);
 	return 0;
 }
